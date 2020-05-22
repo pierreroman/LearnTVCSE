c4ea04ae5213d560c88e8af390b4b208e51b4b7b
Pierre Roman <pierrer@microsoft.com>
2020-05-22 15:42:38 -0400

    az

--- rgCheck.ps1
+++ rgCheck.ps1
@@ -1,6 +1,6 @@
 $rgName=$args[0]
 
-Get-AzureRmResourceGroup -Name $rgName -ev notPresent -ea 0  | Out-Null
+Get-AzResourceGroup -Name $rgName -ev notPresent -ea 0  | Out-Null
 
 if ($notPresent)
 {
